1aaf0d751e58b513ec41e85babc363c737dd023e
mihais98 <mihai.sambotelecan98@gmail.com>
2020-05-20 23:50:13 +0300

    all

--- RegistrationAndLogin/Crypto.cs
+++ RegistrationAndLogin/Crypto.cs
@@ -8,11 +8,11 @@ namespace RegistrationAndLogin
 {
     public static class Crypto
     {
-        public static string Hash(string pass)
+        public static string Hash(string value)
         {
             return Convert.ToBase64String(
                 System.Security.Cryptography.SHA256.Create()
-                .ComputeHash(Encoding.UTF8.GetBytes(pass))
+                .ComputeHash(Encoding.UTF8.GetBytes(value))
                 );
         }
     }
